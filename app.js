f2d22efad782f21db55ff31e3b2fb213458a9980
domwoe <dwoerner@ethz.ch>
2015-02-19 20:06:12 +0100

    prediction

--- app.js
+++ app.js
@@ -41,6 +41,8 @@ var fbGatewayRef = null;
 
 var saveTimeout = null;
 
+var wait = 0;
+
 var HOST = config.HOST;
 var HTTPPORT = config.HTTPPORT;
 var FIREBASE_SECRET = config.FIREBASE_SECRET;
@@ -167,6 +169,7 @@ function watchThermostats(fbHomeRef) {
 
     var thermostats = {};
     fbHomeRef.child('thermostats').on('child_added', function(fbThermostat) {
+        wait++;
         var fbThermostatRef = fbThermostat.ref();
         var thermostatId = fbThermostat.name();
 
@@ -177,8 +180,12 @@ function watchThermostats(fbHomeRef) {
                 clearTimeout(saveTimeout);
             }
 
-            saveTimeout = setTimeout(saveConfig,5000);
+            saveTimeout = setTimeout(saveConfig,30000);
         }
+
+        setTimeout( function() {
+
+        wait--;
 
         //log.info({home: this.homeId, room: this.id}, ' Room: new Thermostat ' + thermostatId);
         thermostats[thermostatId] = new Thermostat(homeId, thermostatId);
@@ -207,6 +214,8 @@ function watchThermostats(fbHomeRef) {
             fbThermostat.child('windowOpnMode').child('Value').val() === 'on ') {
             thermostats[thermostatId].deactivateWindowOpnMode();
         }
+
+    },wait*20000);
 
         // if (saveTimeout) {
         //     clearTimeout(saveTimeout);
